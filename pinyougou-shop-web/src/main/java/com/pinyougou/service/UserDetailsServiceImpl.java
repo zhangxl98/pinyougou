befd1a3ccd00ac1d7e6cb7aba63215bbadb66037
zhangxl98 <651268723@qq.com>
2019-05-10 17:17:25 +0800

    自定义商家登录认证类

---
+++ pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.pinyougou.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author zhangxl98
+ * @Date 5/10/19 4:52 PM
+ * @OS Ubuntu 18.04 LTS
+ * @Device DELL-Inspiron-15-7559
+ * @Modified By
+ * @Version V1.0.0
+ * @Description 商家登录认证类
+ */
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+
+        // 用户在输入密码 123456 时就会通过（用户名随意）
+        return new User(username, "123456", grantedAuthorities);
+    }
+}
