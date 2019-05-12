add4bf487ce098e7266369f195a32674f167e780
zhangxl98 <651268723@qq.com>
2019-05-12 10:04:16 +0800

    用富文本录入商品介绍

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -3,12 +3,16 @@ app.controller('goodsController', function($scope, goodsService) {
 
   // 增加
   $scope.add = () => {
+
+    $scope.entity.goodsDesc.introduction = editor.html()
     goodsService.add($scope.entity).success(
       (response) => {
         if (response.success) {
           alert(response.message)
           // 清空
           $scope.entity = {}
+          // 清空富文本编辑器
+          editor.html('')
         } else {
           alert(response.message)
         }
